a56b4d39b4a46bb7ed232f53299b8f442aea6dbd
Christopher Curtis <ccurtisj@gmail.com>
2013-10-19 12:41:38 -0500

    Add more roles and class types

--- models/class_type.rb
+++ models/class_type.rb
@@ -2,8 +2,18 @@ class ClassType
 
   TYPES_BY_ROLE = {
     'Business Object' => [''],
-    'Creation'        => ['Builder', 'Factory', 'Pool', 'Prototype'],
-    'Interface'       => ['Adapter', 'Interface']
+    'Adaptor'         => ['Adapter', 'Interface'],
+    'Creation'        => ['Builder', 'Director', 'Factory', 'Pool', 'Prototype'],
+    'Events'          => ['Observer', 'Monitor', 'Reporter', 'Spectator'],
+    'Decoration'      => ['able', 'Decorator'],
+    'Delegation'      => ['Agent', 'Delegate', 'Strategy'],
+    'Interface'       => ['Adapter', 'Interface'],
+    'Implementation'  => ['Command', 'Strategy'],
+    'Job Queue'       => ['Job', 'Task', 'Worker'],
+    'Mixin'           => ['able'],
+    'Messaging'       => ['Dispatch', 'Reporter', 'Queue'],
+    'Proxy'           => ['Agent', 'Proxy'],
+    'Structure'       => ['Composite', 'Iterator']
   }
 
   def self.roles
